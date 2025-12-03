764bae9e0837f35c8d45d373eea6c025ece63b1f
maxwell cofie <maxcofie@gmail.com>
2025-12-03 09:16:26 +0000

    Ui clean up

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     source: "./(.*)",
-    headers: [
-        {
-            key: "Content-Security-Policy",
-            value: [
-                "default-src 'self'",
-                "img-src 'self' data: https:",
-                "style-src 'self' 'unsafe-inline' https:",
-                "script-src 'self' 'unsafe-eval' 'unsafe-inline' https:",
-                "connect-src 'self' https://*.supabase.co", // allow Supabase Auth & REST
-            ].join("; "),
-        },
-    ],
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "Content-Security-Policy",
+                        value: [
+                            "default-src 'self'",
+                            "img-src 'self' data: https:",
+                            "style-src 'self' 'unsafe-inline' https:",
+                            "script-src 'self' 'unsafe-eval' 'unsafe-inline' https:",
+                            "connect-src 'self' https://*.supabase.co", // allow Supabase Auth & REST
+                        ].join("; "),
+                    },
+                ],
+            },
+        ];
+    },
     images: {
         remotePatterns: [
             {

e5d07df4f3773f60a4c5810fb3351111d5b4dcf0
maxwell cofie <maxcofie@gmail.com>
2026-02-26 23:12:47 +0000

    Added provider registration form

--- src/components/perk-landing/PerkNFC.tsx
+++ src/components/perk-landing/PerkNFC.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import Link from "next/link";
 import { CreditCard, ShieldCheck, Cpu, Zap, ArrowRight } from "lucide-react";
 
 export function PerkNFC() {
